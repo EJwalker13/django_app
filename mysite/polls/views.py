b809c2bc3dc5610f85b269424c9be10298d9924f
Eli Walker <ewalker2@stu.jsu.edu>
2023-01-11 22:58:09 -0600

    Added a detail.html page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Imported the class HttpResponse from the django and http modules
 from django.http import HttpResponse
-from django.template import loader
 
 from .models import Question
 
@@ -12,7 +12,11 @@ def index(request):
     return HttpResponse("Hello, world. Welcome to Eli's polls index.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -23,8 +27,5 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5] #this orders the fields in the 'Question' model by the publication date 
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
