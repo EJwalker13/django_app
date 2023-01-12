0cb20075e648d876d2d8ad8e7fbde8f04cd30278
Eli Walker <ewalker2@stu.jsu.edu>
2023-01-11 23:29:00 -0600

    No comment

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from . import views #Imported and accessed the polls.views file
 
 #In views I created this index function so now this function takes my index function
 ##and prints my written string variable on the polls website by following the path.
+app_names = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'), # this path connects the detail function in views.py 
